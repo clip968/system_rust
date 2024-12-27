5f68a364b44ae66bf8236a491c597fc0355d2e44
clip968 <clip968@gmail.com>
2024-12-27 16:36:35 +0900

    12 27

--- new_struct/src/main.rs
+++ new_struct/src/main.rs
@@ -71,6 +71,36 @@ impl Score {
         Score {score : score}
     }
 }
+
+struct Node {
+    value : i32, // 노드의 값을 저장하는 value
+    next : Option<Box<Node>>, // 다음 노드를 가리키는 필드, option을 사용해 노드가 없을 수도 있는 상황을 대비(마지막 노드)
+}
+
+impl Node {
+    fn append(&mut self, elem:i32){
+        match self.next {
+            Some(ref mut next) => {
+                next.append(elem);
+            }
+            None => {
+                let node = Node {
+                    value : elem,
+                    next : None,
+                };
+                self.next = Some(Box::new(node))
+            }
+        }
+    }
+    fn list(&self) {
+        print!("{}", self.value);
+        match self.next {
+            Some(ref next) => next.list(),
+            None => {}
+        }
+    }
+}
+
 fn main() {
     // println!("학번을 입력해주세요.");
     // let mut id = String::new();
@@ -115,12 +145,40 @@ fn main() {
     //         port: 8080,
     //     }
 
-    let m = Message::Put(String::from("/root/"));
-    m.execute();
+    // let m = Message::Put(String::from("/root/"));
+    // m.execute();
 
-    let m = Message::List(33);
-    m.execute();
+    // let m = Message::List(33);
+    // m.execute();
+    // Option 열거형은 값이 있을 수도 있고 없을 수도 있는 경우 사용되는 열거형이다.
+    // let some_number = Some(99);
+    // let some_string = Some("러스트");
+    // let can_be_none : Option<i32> = None;
 
+    // let some_string = Some(String::from("러스트"));
+    // let none_string: Option<String> = None;
+
+    // print_option(some_string);
+    // print_option(none_string);
+
+    let mut head = Node {
+        value:1,
+        next:None,
+    };
+
+    for i in 2..10{
+        head.append(i);
+    }
+
+    head.list();
+
+}
+
+fn print_option(val: Option<String>){
+    match val {
+        Some(val ) => println!("{}", val),
+        None => println!("None"),
+    }
 }
 
 #[test]
